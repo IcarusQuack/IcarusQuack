2f8ad68a858da35adce00d84822ed0da5a16cb6f
IcarusQuack <penguin54687@gmail.com>
2021-03-01 21:30:30 -0500

    fixed froge

---
+++ src/pages/frogs.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+import Layout from '../components/layout'
+
+const Frogs = () => (
+  <Layout>
+    <h1>These are my frogs, Chicken & Waffles</h1>
+    <p>froge</p>
+    <Link to="/">Go back to the homepage</Link>
+  </Layout>
+)
+
+export default Frogs
